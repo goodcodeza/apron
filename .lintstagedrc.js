b64878702ccccbce3fed657181e339b7fb1cb0fc
Nolan Pather <goodcodeza@gmail.com>
2024-10-07 19:59:00 +0100

    chore: format all

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,22 +1,18 @@
-const path = require("path");
+const path = require('path');
 
 const buildPrettierCommand = (filenames) => {
-  const fixables = filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" ");
+	const fixables = filenames.map((f) => path.relative(process.cwd(), f)).join(' ');
 
-  return `prettier --ignore-unknown --write ${fixables}`;
+	return `prettier --ignore-unknown --write ${fixables}`;
 };
 
 const buildEslintCommand = (filenames) => {
-  const fixables = filenames
-    .map((f) => `--file ${path.relative(process.cwd(), f)}`)
-    .join(" ");
+	const fixables = filenames.map((f) => `--file ${path.relative(process.cwd(), f)}`).join(' ');
 
-  return `next lint --fix ${fixables}`;
+	return `next lint --fix ${fixables}`;
 };
 
 module.exports = {
-  "*": [buildPrettierCommand],
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+	'*': [buildPrettierCommand],
+	'*.{js,jsx,ts,tsx}': [buildEslintCommand]
 };
